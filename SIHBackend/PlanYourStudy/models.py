f7a9dd8d9330c829015cb0f6902ae705c60f14ee
ana301 <keshariananya@gmail.com>
2018-03-30 16:28:44 +0530

    Nav bar changed a lil

--- SIHBackend/PlanYourStudy/models.py
+++ SIHBackend/PlanYourStudy/models.py
@@ -12,6 +12,6 @@ class Category(models.Model):
 		('Government','Government'),
 		('Private','Private'),)
 	InstituteType=models.CharField(max_length=100,choices=CHOICES,default="Government")
-	Check = models.BooleanField(default=True)
+	Check = models.BooleanField(default=False)
     
 # Create your models here.
